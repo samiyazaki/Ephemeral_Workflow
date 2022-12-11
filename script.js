c1616c9665117928b1d0c193d45ce71c0388c800
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-12-11 17:12:08 -0500

    Adding more comments

--- script.js
+++ script.js
@@ -1,59 +1,43 @@
 $(function () {
-  var now = dayjs().format("dddd: MMM D, YYYY; h:mm A"); // displays as Sunday: Dec 11, 2022; 4:27 PM or whatever time it is for you now. I don't judge
-  document.getElementById("currentDay").innerHTML = now; //allows text to display in the "currentDay" element by id
-  
+  let currDay = document.getElementById("currentDay"); // Aligning the time clock with the currentDay id
+  currDay.innerHTML = dayjs().format("ddd: MMM D, YYYY; h:mm A"); //Displays as Sunday: Dec 11, 2022; 4:27 PM or whatever time it is for you now. I don't judge
+
+  let myFunction = () => {
+    currDay.innerHTML = dayjs().format("ddd: MMM D, YYYY; h:mm A"); //Allows text to display in the "currentDay" element by id
+  };
+  window.setInterval(myFunction, 2000);
+
   var init = false;
-  let kiefer24 = dayjs().format("H");
-  let chiwetel12 = dayjs().format("h");
+  let kiefer24 = dayjs().format("H"); //Setting 24 hour clock
+  let chiwetel12 = dayjs().format("h"); //Setting 12 hour clock
   if (init) {
     kiefer24 = 13;
     chiwetel12 = 1;
   }
   var cal = new Date();
-  var currentHour = cal.getHours();
+  var currentHour = cal.getHours(); //Setting up the framework to determine if the current time is Past, present, or future
 
-  function randomInRange(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-
-  confetti({
-    angle: randomInRange(300, 300),
-    spread: randomInRange(700, 700),
-    particleCount: randomInRange(500, 500),
-    origin: { y: 0.1 },
-  });
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   $(".saveBtn").on("click", function () {
-    /* var hr9 = document.getElementById('9').value;
-        localStorage.setItem('text9', hr9); */
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, value);
-    console.log(time, value);
-    function randomInRange(min, max) {
-      return Math.random() * (max - min) + min;
-    }
+    //When you click the save button...
+    var value = $(this).siblings(".description").val(); //Originally I had a save value for each individual time block... When a wonderful teacher showed me how to use sibling selectors to search through the DOM elements and construct a jQuery object from the matching elements
+    var time = $(this).parent().attr("id"); //Stepping up one level for the parent in the DOM tree and finding the attribute "id"
+    localStorage.setItem(time, value); // Setting the local storage values from the previous  variables of value and time
+    //console.log(time, value); This was for testing. I'm leaving it because this is where I broke the most.
 
+    // Do you like confetti?
     confetti({
-      angle: randomInRange(300, 300),
-      spread: randomInRange(700, 700),
-      particleCount: randomInRange(250, 500),
+      // This is how you call the confetti function. My life is empty but I can make confetti appear at the click of a button, so I've got that going for me.
+      angle: 300,
+      spread: 700,
+      particleCount: 500,
       origin: { y: 0.1 },
     });
+    //Oh yeah I added an audio play on button click. Is it necessary? No. Is it useful? No. Should I have included it? Probably not.
     var clickAudio = new Audio("./assets/mixkit-fairy-arcade-sparkle-866.wav");
     clickAudio.play();
     return;
   });
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
   for (var i = 9; i < 18; i++) {
     if (i < currentHour) {
       document.getElementById(i.toString()).classList.add("past");
@@ -66,15 +50,15 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  $('#hour9 .description').val(localStorage.getItem('hour9'));
-  $('#hour10 .description').val(localStorage.getItem('hour10'));
-  $('#hour11 .description').val(localStorage.getItem('hour11'));
-  $('#hour12 .description').val(localStorage.getItem('hour12'));
-  $('#hour13 .description').val(localStorage.getItem('hour13'));
-  $('#hour14 .description').val(localStorage.getItem('hour14'));
-  $('#hour15 .description').val(localStorage.getItem('hour15'));
-  $('#hour16 .description').val(localStorage.getItem('hour16'));
-  $('#hour17 .description').val(localStorage.getItem('hour17'));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
   //$('#hour-10.description').val(localStorage.getItem('hour-10'));
   // $('#hour-11.description').val(localStorage.getItem('hour-11'));
   /*var saved9 = localStorage.getItem('text9');
